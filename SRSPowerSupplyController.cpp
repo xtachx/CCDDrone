9e79e17298cbad7bd28597aa85d4e759a959ac4f
Alexander Piers <apiers@uw.edu>
2020-07-28 08:50:44 -0700

    add - core operation to set power and output state of supply

---
+++ SRSPowerSupplyController.cpp
@@ -0,0 +1,95 @@
+#include "SRSPowerSupplyController.hpp"
+
+SRSPowerSupplyController::SRSPowerSupplyController(){
+	this->currentVoltage = this->GetPSVoltage();
+    this->currentOutputStatus = this->GetP
+    printf("Dummy instance of class. No serial communication available");
+}
+
+SRSPowerSupplyController::SRSPowerSupplyController(std::string SerialPort) : SerialDevice(SerialPort){
+	 /* Set Baud Rate */
+        cfsetospeed (&this->tty, (speed_t)B115200);
+        cfsetispeed (&this->tty, (speed_t)B115200);
+
+        /* Setting other Port Stuff */
+        // Found that the defaults in tty worked best for arduino communication
+
+        /* Flush Port, then applies attributes */
+        tcflush( USB, TCIFLUSH );
+        if ( tcsetattr ( USB, TCSANOW, &tty ) != 0)
+        {
+            std::cout << "Error " << errno << " from tcsetattr" << std::endl;
+        }
+
+
+        this->currentVoltage = this->GetPSVoltage();
+        this->currentOutputStatus = this->GetPSOutput();
+        this->WatchdogFuse = 1;
+        this->valveState = 0;
+        this->timeBetweenFillState = 0;
+        this->overflowVoltage = 0;
+        this->timeInCurrentState = 0;
+        this->isOverflow = 0;
+
+        printf("SRS Power Supply is now ready to accept instructions.\n");
+}
+
+SRSPowerSupplyController::~SRSPowerSupplyController(){
+	close(USB);
+}
+
+double SRSPowerSupplyController::ReadPSVoltage(){
+	std::string srsCmd = "SOUT?";
+
+	this->WriteString(srsCmd);
+	std::string srsReturn = this->ReadLine();
+
+	 try {
+        this->currentOutputStatus = std::stoi(srsReturn);
+    } catch(...){
+        printf("Error in ReadPSOutput\n");
+    }
+
+    return this->currentOutputStatus;
+}
+
+bool SRSPowerSupplyController::ReadPSOutput(){
+
+	std::string srsCmd = "SOUT?";
+
+	this->WriteString(srsCmd);
+	std::string srsReturn = this->ReadLine();
+
+	 try {
+        this->currentOutputStatus = std::stoi(srsReturn);
+    } catch(...){
+        printf("Error in ReadPSOutput\n");
+    }
+
+    return this->currentOutputStatus;
+
+}
+
+void SRSPowerSupplyController::WritePSVoltage(double voltage){
+	std::string srsCmd = "VOLT " + std::to_string(voltage);
+
+	// Write to power supply
+	this->WriteString(cmd);
+
+	currentVoltage = this->ReadPSVoltage();
+}
+
+void SRSPowerSupplyController::WritePSOutput(bool output){
+	std::string srsCmd = "SOUT " + std::to_string(output);
+
+	// Write to power supply
+	this->WriteString(cmd);
+
+	currentOutputStatus = this->ReadPSOuput();
+}
+
+void SRSPowerSupplyController::VoltageRamp(double startScanVoltage, double stopScanVoltage, double scanTime, bool display){
+
+	// Define ramp parmaeters
+	std::string sr
+}
--- SRSPowerSupplyController.hpp
+++ SRSPowerSupplyController.hpp
@@ -1,1 +1,28 @@
 #ifndef CCDDRONE_SRSPOWERSUPPLYCONTROLLER_HPP
+#define CCDDRONE_SRSPOWERSUPPLYCONTROLLER_HPP
+
+#include "SerialDeviceT.hpp"
+#include <iostream>
+
+class SRSPowerSupplyController : public SerialDevice
+{
+public:
+	SRSPowerSupplyController();
+	SRSPowerSupplyController(std::string);
+	~SRSPowerSupplyController();
+
+	// Get PS Values
+	double ReadPSVoltage();
+	bool ReadPSOutput();
+
+	// Set PS Values
+	void WritePSVoltage(double voltage);
+	void WritePSOutput(bool output);
+
+
+	void VoltageRamp(double startScanVoltage, double stopScanVoltage, double scanTime, bool display);
+
+	double currentVoltage;
+	bool currentOutputStatus;
+};
+#endif //CCDDRONE_SRSPOWERSUPPLYCONTROLLER_HPP
