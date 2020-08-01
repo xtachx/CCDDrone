75ce0cf6478172556b345bdc039faebe8fae1f49
Alexander Piers <apiers@uw.edu>
2020-08-01 11:32:30 -0700

    add - ramp down voltage script

--- SRSPowerSupplyDrone.cpp
+++ SRSPowerSupplyDrone.cpp
@@ -8,18 +8,22 @@ int main(int argc, char const *argv[])
 	auto * srsSupply = new SRSPowerSupplyController("/dev/ttyUSB3");
 	sleep(0.5);
 
-	double setVoltage = 25;
-	double setVoltageTime = 4;
-	printf("Set power supply to %.2f V. Hold for %.1f seconds.\n", setVoltage, setVoltage);
-	srsSupply->WritePSVoltage(setVoltage);
-	sleep(setVoltageTime);
+	// Ramp down to zero volts over 5 seconds
+	double rampTime = 5;
+	printf("Ramp down to zero volts over %0.1f s....\n", rampTime);
+	srsSupply->VoltageRamp(srsSupply->ReadPSVoltage(), 0, rampTime, true);
 
-	printf("Test Ramp up....\n");
-	srsSupply->VoltageRamp(0, 75, 10, true);
-	sleep(15);
+	// double setVoltage = 25;
+	// double setVoltageTime = 4;
+	// printf("Set power supply to %.2f V. Hold for %.1f seconds.\n", setVoltage, setVoltage);
+	// srsSupply->WritePSVoltage(setVoltage);
+	// sleep(setVoltageTime);
 
-	printf("Test Ramp down....\n");
-	srsSupply->VoltageRamp(srsSupply->ReadPSVoltage(), 0, 10, true);
+	// printf("Test Ramp up....\n");
+	// srsSupply->VoltageRamp(0, 75, 10, true);
+	// sleep(15);
+
+	// srsSupply->VoltageRamp(srsSupply->ReadPSVoltage(), 0, 10, true);
 
 
 	return 0;
